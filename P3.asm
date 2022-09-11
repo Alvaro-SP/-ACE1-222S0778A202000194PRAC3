1414b9d310d8be5c2d119fb73cf1126d3688775a
Alvaro-SP <socop2412@gmail.com>
2022-09-10 22:32:48 -0600

    starting with messages and viewers

--- P3.asm
+++ P3.asm
@@ -1,42 +1,118 @@
-.model small
 
-.stack
+;     █░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀   █▀▄▀█ █▄█   █▀▀ ▄▀█ █▀▄▀█ █▀▀
+;     ▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄   █░▀░█ ░█░   █▄█ █▀█ █░▀░█ ██▄
+; ---------------------------------------------------------------------------------------------------
+.MODEL LARGE
+.386
+.STACK 64
+.DATA
 
-.data
-    var1 db "HELLO WORLD", "$"
-.code
-; MACRO SIRVE PARA REUTILIZAR CODIGO:
-        ; se pueden usar etiquetas y saltos
+;--------------------------  MIS_DATOS -----------------------------
+tb1             DB   38,'BIENVENIDO AL JUEGO BATTLESHIPS !!!!!!'
+tb2             DB   38,'Universidad de San Carlos de Guatemala'
+tb3             DB   22,'Facultad de Ingenieria'
+tb4             DB   30,'Escuela de Ciencias y Sistemas'
+tb5             DB   9,'Seccion A'
+tb6             DB   27,'ALVARO EMMANUEL SOCOP PEREZ'
+tb7             DB   9,'202000194'
+pressenter      DB   24,'ENTER: Para continuar...'
+
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+;--------------------------  MIS_DATOS -----------------------------
+
+KEY_PRESSED                     DB  ?
+WAIT_FOR_THE_OTHER_PLAYER_MSG   DB  45,"- Please wait the other player to press ENTER" 
+MY_INDEX                        EQU 1
+OTHER_PLAYER_INDEX              EQU 2
+WAIT_TO_RECEIEVE_ATTACK_COORD   DB  28,"WAIT TO RECEIEVE ATTACK COORD"
+WAIT_TO_RECEIEVE_ATTACK_RESULT   DB  29,"WAIT TO RECEIEVE ATTACK RESULT"
+ATTACKX_STRING        DB  2,?,?
+ATTACKY_STRING        DB  1,?
+
+;████████████████████████████ SEGMENTO DE CODIGO ████████████████████████████
+.CODE
+;                ░█▀▄▀█ ─█▀▀█ ░█▀▀█ ░█▀▀█ ░█▀▀▀█ ░█▀▀▀█ 
+;                ░█░█░█ ░█▄▄█ ░█─── ░█▄▄▀ ░█──░█ ─▀▀▀▄▄ 
+;                ░█──░█ ░█─░█ ░█▄▄█ ░█─░█ ░█▄▄▄█ ░█▄▄▄█
+print MACRO MYMESSAGE , LOCATION,COLOR
+	PUSHA
+	MOV DX,LOCATION
+	MOV BP, OFFSET MYMESSAGE
+	MOV SI, COLOR
+	CALL  print_
+	POPA
+ENDM print
+
+misdatos MACRO
+	PUSHA
+	CALL misdatos_
+	POPA
+ENDM misdatos
 
 
-imprimir macro texto
-    mov ah, 09
-    mov dx, offset texto
-    int 21h
-endm
+; ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬      █▀▄▀█ ▄▀█ █ █▄░█       ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+; ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬      █░▀░█ █▀█ █ █░▀█       ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+main PROC FAR
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV ES, AX
+    misdatos
+
+    HLT ; para decirle al CPU que se estara ejecutando varias veces (detiene CPU hasta sig interrupcion)
+    RET
+main    ENDP
 
 
 
-main proc
-    mov ax, @data
-    mov ds, ax
-    ; se usan interrupciones para mostrar cosas
-    ; http://arantxa.ii.uam.es/~gdrivera/labetcii/int_dos.htm
 
-    imprimir var1
-    mov ah, 09
-    mov dx, offset var1
-    int 21h
+;☻ ===================== METODO MOSTRAR DATOS ======================= ☻
+misdatos_     PROC NEAR
+    MOV AX,4F02H           ;SETEAMOS EL MODO VIDEO INT 10   800*600
+    MOV BX,103H
+    INT 10H
+    ; imprimo el texto de inicio
+    print tb1 , 0820H , 0FF28H
+    print tb2 , 0F10h , 0FF0FH
+    print tb3 , 1210H , 0FF0FH
+    print tb4 , 1410H , 0FF0FH
+    print tb5 , 1610H , 0FF0FH
+    print tb6 , 1810H , 0FF0FH
+    print tb7 , 1A10H , 0FF0FH
+    print pressenter , 2125H , 0FF0FH
+    RET
+misdatos_     ENDP
 
-    ;SALIDA DEL PROGRAMA
-    MOV AX , 4c000h
-    AX = 4C00H ; siempre al finalizar el programa.
 
-main endp
-end main
 
-;EJECUTAR DOS BOX
-; Se monta el Bin en disco local C
+;☻ ===================== METODO IMPRIMIR ======================= ☻
+print_    PROC NEAR
+    MOV AX,1301H
+    MOV BX,BP
+    MOV CL,[BX]
+    MOV CH,00H
+    ADD BP,1H
+    MOV BX,SI
+    INT 10H
+    RET
+print_    ENDP
 
-; ML.EXE <== para compilar el programa.
-; nl ejemploAux.ASM  
+
+end     MAIN
+
+;  ░█████╗░██╗░░░░░██╗░░░██╗░█████╗░██████╗░░█████╗░    ░██████╗░█████╗░░█████╗░░█████╗░██████╗░
+;  ██╔══██╗██║░░░░░██║░░░██║██╔══██╗██╔══██╗██╔══██╗    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+;  ███████║██║░░░░░╚██╗░██╔╝███████║██████╔╝██║░░██║    ╚█████╗░██║░░██║██║░░╚═╝██║░░██║██████╔╝
+;  ██╔══██║██║░░░░░░╚████╔╝░██╔══██║██╔══██╗██║░░██║    ░╚═══██╗██║░░██║██║░░██╗██║░░██║██╔═══╝░
+;  ██║░░██║███████╗░░╚██╔╝░░██║░░██║██║░░██║╚█████╔╝    ██████╔╝╚█████╔╝╚█████╔╝╚█████╔╝██║░░░░░
+;  ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░    ╚═════╝░░╚════╝░░╚════╝░░╚════╝░╚═╝░░░░░
